47ea816ed01dbb8e57b9d502f7c0c786d9583293
mariel-uykim <marielanne.uykim@students.mq.edu.au>
2022-03-26 20:13:03 +1100

    added comments and organised certain sections

--- DSClient/src/DSClient.java
+++ DSClient/src/DSClient.java
@@ -1,6 +1,5 @@
 import java.net.*;
 import java.io.*;
-import java.util.Arrays;
 
 public class DSClient {
     private static Socket s;
@@ -31,22 +30,24 @@ public class DSClient {
     public static Boolean initialise() throws IOException{
         sendMsg("HELO");
 
-        if(printRes().equals("OK")) {
+        if(printRes(true).equals("OK")) {
             sendMsg("AUTH " + username + "\n");
         }
 
-        if(printRes().equals("OK")){
+        if(printRes(false).equals("OK")){
             return true;
         }
         return false;
     }
 
-    //printRes(): gets and prints out received message
-    public static String printRes() throws IOException {
+    //printRes(): gets server response and prints out received message if print=true
+    public static String printRes(Boolean print) throws IOException {
         String response = "";
         response = input.readLine();
 
-        System.out.println("SERVER: " + response);
+        if(print) {
+            System.out.println("SERVER: " + response);
+        }
         return response;
     }
 
@@ -69,9 +70,10 @@ public class DSClient {
         }
     }
 
-    //getInfo()): get data from jobn response if 1 or all if 0, 
-    //returns number of data
-    public static int getInfo(String response, int option) throws IOException{
+    //getInfo(): get data from jobn response if 1 or all if 0, returns
+    //number of data. If option is 1, gets servers with specific capabilities
+    //if 2, gets all server
+    public static int getInfo(String response, int option) throws IOException {
         if(option == 1) {
             //split response string into several parts
             String [] res = response.split(" ", 7);
@@ -87,7 +89,7 @@ public class DSClient {
         else if(option == 2) {
             sendMsg("GETS All");
         }
-        String data = printRes();
+        String data = printRes(false);
         
         //split data if not error
         if(data.contains("DATA")){
@@ -103,11 +105,33 @@ public class DSClient {
         return -1;
     }
 
-    //getLargestServer(): gets Largest server (last on list)
-    public static String [] getLargestServer(String data) {
+    //getServers(): stores server information 
+    public static String getAllServers(int nData) throws IOException {
+        //stores response on server information
+        String reply = null;
+        String serverInfo = " ";
+
+        for(int i = 0; i < nData; i++) {
+            reply = input.readLine();
+            System.out.println(reply);
+            serverInfo += ("--" + reply);
+        }
+
+        sendMsg("OK");
+
+        return serverInfo;
+
+    }
+
+    //getLargestServer(): Calls on to getAllServers to see all servers, 
+    //and returns Largest server (last on list)
+    public static String [] getLargestServer(int nData) throws IOException {
+        String data = getAllServers(nData);
+
         String [] list = data.split("--");
         String largest = list[list.length-1];
-        String [] serverInfo = largest.split(" "); 
+        String [] serverInfo = largest.split(" ");
+        
         return serverInfo;
     }
 
@@ -123,6 +147,7 @@ public class DSClient {
         String [] data = response.split(" ");
         return Integer.parseInt(data[2]);
     }
+
     //DSClient(): Class instantiates server connection and communicates
     //with DS Server 
     public DSClient(String hostid, int port) {
@@ -130,70 +155,77 @@ public class DSClient {
         //try to connect to server
         Boolean result = serverConn(hostid, port);
 
+        //exit if failed to connect
         if(result == false) {
+
+            System.out.println("Failed to conencect..");
             return;
         }
 
         try {
+
+            //starts connection and authenticates with server
             if(!initialise()) {
                 return;
             }
 
+            //tells server 'redy' to start sending commands
             sendMsg("REDY");
-            String jobn = printRes();
+
+            //stores server response in jobn variable
+            String jobn = printRes(false);
 
             //get list of servers
             int nData = getInfo(jobn, 2);
 
-            //quit if no servers or error
+            //quit if no servers or an error happens
             if(nData == -1) {
                 sendMsg("QUIT");
             }
             
-            sendMsg("OK");
-            String reply = null;
-            String serverInfo = " ";
-
-            for(int i = 0; i < nData; i++) {
-                reply = input.readLine();
-                serverInfo += ("--" + reply);
-            }
-
+            //acknowleges server repsonse
             sendMsg("OK");
 
-            //store data in server array
-            String [] server = getLargestServer(serverInfo);
-            System.out.println("SERVER: " + Arrays.toString(server));
-            printRes();
-            
+            //gets largest server and stores in array
+            String [] server = getLargestServer(nData);
+
+            //clear input stream
+            printRes(false);
+
             int i = 0;
             while(true) {
-                //restarts i 
+                //restarts i when it's more than the highest server id
                 if(i>Integer.parseInt(server[1])) {
                     i = 0;
                 }
+
                 //skips current iteration when server sends JCPL data
                 if(jobn.contains("JCPL")) {
                     sendMsg("REDY");
-                    jobn = printRes();
+                    jobn = printRes(false);
                     if(jobn.contains("NONE")) {
                         sendMsg("QUIT");
                         break;
                     }
                     continue;
                 }
+
+                //sends a schedule to server consisting of current job id, the server type,
+                //and server id
                 String sched = "SCHD " + getJobID(jobn) + " " + server[0] + " " + i;
                 System.out.println("SCHED: " + sched);
                 sendMsg(sched);
-                i++;
 
-                printRes();
+                //increment i
+                i++;
+                
+                printRes(false);
 
                 sendMsg("REDY");
 
-                jobn = printRes();
-                System.out.println("JOB ID: " + jobn);
+                jobn = printRes(false);
 
+                //end loop when no more jobs
                 if(jobn.contains("NONE")) {
                     sendMsg("QUIT");
                     break;
@@ -207,6 +239,7 @@ public class DSClient {
             System.out.println(e);
         }
 
+        //ends connection
         closeConn();
     }
 
